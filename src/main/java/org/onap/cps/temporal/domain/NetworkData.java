ec3d5d827412a7c4744063980769ffb99d74b6a5
puthuparambil.aditya <aditya.puthuparambil@bell.ca>
2021-05-06 16:12:44 +0100

    Implement service and repository layers for storing temporal data
    1. Basic structure created
    2. Basic tests added
    3. Jacoco aggregator added to include spock test in coverage check
    
    Issue-ID: CPS-194
    Signed-off-by: puthuparambil.aditya <aditya.puthuparambil@bell.ca>
    Change-Id: Icf23c2e647106f7985dff14d9901806f7c4aa55a

---
+++ src/main/java/org/onap/cps/temporal/domain/NetworkData.java
@@ -0,0 +1,80 @@
+/*
+ * ============LICENSE_START=======================================================
+ * Copyright (c) 2021 Bell Canada.
+ * ================================================================================
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *        http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ * ============LICENSE_END=========================================================
+ */
+
+package org.onap.cps.temporal.domain;
+
+import com.vladmihalcea.hibernate.type.json.JsonBinaryType;
+import java.io.Serializable;
+import java.time.OffsetDateTime;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.IdClass;
+import javax.persistence.Table;
+import javax.validation.constraints.NotNull;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.Type;
+import org.hibernate.annotations.TypeDef;
+
+/**
+ * Entity to store an anchor configuration or state along with the moment it has been observed.
+ */
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@IdClass(NetworkDataID.class)
+@Builder
+@Entity
+@Table(name = "network_data")
+@TypeDef(name = "jsonb", typeClass = JsonBinaryType.class)
+public class NetworkData implements Serializable {
+
+    private static final long serialVersionUID = 1L;
+
+    @Id
+    @Column(name = "timestamp")
+    private OffsetDateTime observedTimestamp;
+
+    @Id
+    @Column
+    private String dataspace;
+
+    @Id
+    @Column
+    private String anchor;
+
+    @NotNull
+    @Column
+    private String schemaSet;
+
+    @NotNull
+    @Type(type = "jsonb")
+    @Column(columnDefinition = "jsonb")
+    private String payload;
+
+    @CreationTimestamp
+    @Column(name = "version", updatable = false)
+    private OffsetDateTime creationTimestamp;
+
+}
