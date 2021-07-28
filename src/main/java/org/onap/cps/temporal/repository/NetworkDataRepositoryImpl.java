38869892d60309d2571702514caca53e9a6feecf
Renu Kumari <renu.kumari@bell.ca>
2021-07-28 00:07:41 -0400

    service and repository layer to fetch data by search criteria
    Issue-ID: CPS-375
    Signed-off-by: Renu Kumari <renu.kumari@bell.ca>
    Change-Id: Iedac0e48b2391a60f3eb9ab710ccdff0c9185407

---
+++ src/main/java/org/onap/cps/temporal/repository/NetworkDataRepositoryImpl.java
@@ -0,0 +1,177 @@
+/*
+ * ============LICENSE_START=======================================================
+ * Copyright (c) 2021 Bell Canada.
+ * ================================================================================
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *         http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ * ============LICENSE_END=========================================================
+ */
+
+package org.onap.cps.temporal.repository;
+
+import java.math.BigInteger;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+import javax.persistence.Query;
+import lombok.Getter;
+import lombok.extern.slf4j.Slf4j;
+import org.apache.commons.lang3.StringUtils;
+import org.onap.cps.temporal.domain.NetworkData;
+import org.onap.cps.temporal.domain.SearchCriteria;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageImpl;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
+import org.springframework.stereotype.Repository;
+
+@Repository
+@Slf4j
+public class NetworkDataRepositoryImpl implements NetworkDataQueryRespository {
+
+    @PersistenceContext
+    private EntityManager entityManager;
+
+    @Override
+    public Page<NetworkData> findBySearchCriteria(final SearchCriteria searchCriteria) {
+
+        final var searchCriteriaQueryBuilder = new SearchCriteriaQueryBuilder(searchCriteria);
+        searchCriteriaQueryBuilder.buildQuery();
+
+        final long count = getTotalRowCount(searchCriteriaQueryBuilder.getCountNativeQuery(),
+            searchCriteriaQueryBuilder.getQueryParameters());
+
+        final List<NetworkData> data = getNetworkDataList(searchCriteriaQueryBuilder.getDataNativeQuery(),
+            searchCriteriaQueryBuilder.getQueryParameters(), searchCriteria.getPageable());
+
+        return new PageImpl<>(data, searchCriteria.getPageable(), count);
+    }
+
+    private long getTotalRowCount(final String nativeCountQuery, final Map<String, Object> queryParameters) {
+        final Query countQuery = entityManager.createNativeQuery(nativeCountQuery);
+        queryParameters.forEach(countQuery::setParameter);
+        return ((BigInteger) countQuery.getSingleResult()).longValue();
+    }
+
+    private List<NetworkData> getNetworkDataList(final String nativeDataQuery,
+        final Map<String, Object> queryParameters, final Pageable pageable) {
+        final Query dataQuery = entityManager.createNativeQuery(nativeDataQuery, NetworkData.class);
+        queryParameters.forEach(dataQuery::setParameter);
+        dataQuery.setFirstResult(Math.toIntExact(pageable.getOffset()));
+        dataQuery.setMaxResults(pageable.getPageSize());
+        return dataQuery.getResultList();
+    }
+
+
+    // TODO It is not an builder
+    private static class SearchCriteriaQueryBuilder {
+
+        @Getter
+        private Map<String, Object> queryParameters = new HashMap<>();
+        private StringBuilder queryBuilder = new StringBuilder();
+
+        private String countQueryCondition;
+        private String dataQueryCondition;
+
+        private final SearchCriteria searchCriteria;
+
+        SearchCriteriaQueryBuilder(final SearchCriteria searchCriteria) {
+            this.searchCriteria = searchCriteria;
+        }
+
+        private void buildQuery() {
+
+            queryBuilder.append(" WHERE dataspace = :dataspace ");
+            queryParameters.put("dataspace", searchCriteria.getDataspaceName());
+
+            addAnchorsCondition();
+            addSchemaSetCondition();
+            addObservedAfterCondition();
+            addSimplePayloadCondition();
+
+            addPointInTimeCondition();
+            countQueryCondition = queryBuilder.toString();
+
+            orderBy();
+            dataQueryCondition = queryBuilder.toString();
+            //addPagination();
+
+        }
+
+
+        private void addSchemaSetCondition() {
+            if (!StringUtils.isEmpty(searchCriteria.getSchemaSetName())) {
+                queryBuilder.append(" AND schema_set = :schemaSetName ");
+                queryParameters.put("schemaSetName", searchCriteria.getSchemaSetName());
+            }
+        }
+
+        private void addAnchorsCondition() {
+            if (!searchCriteria.getAnchorNames().isEmpty()) {
+                queryBuilder.append(" AND anchor in :anchorNames");
+                queryParameters.put("anchorNames", searchCriteria.getAnchorNames());
+            }
+        }
+
+        private void addSimplePayloadCondition() {
+            if (!StringUtils.isEmpty(searchCriteria.getSimplePayloadFilter())) {
+                queryBuilder.append(" AND payload @> :simplePayloadFilter\\:\\:jsonb ");
+                queryParameters.put("simplePayloadFilter", searchCriteria.getSimplePayloadFilter());
+            }
+        }
+
+        private void addPointInTimeCondition() {
+            if (searchCriteria.getPointInTime() != null) {
+                queryBuilder.append(" AND created_timestamp <= :pointInTime");
+                queryParameters.put("pointInTime", searchCriteria.getPointInTime());
+            }
+        }
+
+        private void addObservedAfterCondition() {
+            if (searchCriteria.getAfter() != null) {
+                queryBuilder.append(" AND observed_timestamp >= :after");
+                queryParameters.put("after", searchCriteria.getAfter());
+            }
+        }
+
+        private void orderBy() {
+            final Sort sortBy = searchCriteria.getPageable().getSort();
+            queryBuilder.append(" order by ");
+            boolean isFirst = true;
+            for (final Sort.Order order : sortBy) {
+                if (!isFirst) {
+                    queryBuilder.append(", ");
+                } else {
+                    isFirst = false;
+                }
+                final String direction = order.isAscending() ? "asc" : "desc";
+                queryBuilder.append(order.getProperty() + " " + direction);
+            }
+        }
+
+        String getDataNativeQuery() {
+            return "select * from network_data nd " + dataQueryCondition;
+        }
+
+        String getCountNativeQuery() {
+            return "select count(1) from network_data nd " + countQueryCondition;
+        }
+
+    }
+
+
+}
+
--- src/test/groovy/org/onap/cps/temporal/service/NetworkDataServiceImplSpec.groovy
+++ src/test/groovy/org/onap/cps/temporal/service/NetworkDataServiceImplSpec.groovy
@@ -13,12 +13,17 @@
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
  * limitations under the License.
+ *
+ * SPDX-License-Identifier: Apache-2.0
  * ============LICENSE_END=========================================================
  */
 
 package org.onap.cps.temporal.service
 
 import org.onap.cps.temporal.domain.NetworkDataId
+import org.onap.cps.temporal.domain.SearchCriteria
+import org.springframework.data.domain.Page
+import org.springframework.data.domain.PageImpl
 
 import java.time.OffsetDateTime
 import org.onap.cps.temporal.domain.NetworkData
@@ -63,6 +68,25 @@ class NetworkDataServiceImplSpec extends Specification {
             objectUnderTest.addNetworkData(networkData)
         then: 'network service exception is thrown'
             thrown(ServiceException)
+    }
+
+    def 'Query network data by search criteria.'() {
+        given: 'search criteria'
+            def searchCriteria = SearchCriteria.builder()
+                    .dataspaceName('my-dataspaceName')
+                    .schemaSetName('my-schemaset')
+                    .pagination(0, 10)
+                    .build()
+        and: 'response to be returned from repository'
+            def responseFromRepository = new PageImpl<>(Collections.emptyList(),searchCriteria.getPageable(), 10)
+
+        when: 'search is executed'
+            def resultPage = objectUnderTest.searchNetworkData(searchCriteria)
+
+        then: 'data is fetched from repository and returned'
+             1 * mockNetworkDataRepository.findBySearchCriteria(searchCriteria) >> responseFromRepository
+            resultPage == responseFromRepository
+
     }
 
 }
