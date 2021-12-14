8b9d4da43937a160c335036833cdcf37e2dd4edd
ToineSiebelink <toine.siebelink@est.tech>
2021-12-14 09:06:42 +0000

    Add support for delete event operation
    (copy Toine)
    - Added new column operation in DB
    - Updated existing rows to have 'UPDATE' value for operation field
    - Added ability to process both V1 and V2 event schema
    - Changed code and testcase to support operation field
    
    Issue-ID: CPS-790
    
    Change-Id: I3ad4c27388138f34faba797fb335e296519d2f6b
    Signed-off-by: ToineSiebelink <toine.siebelink@est.tech>

--- src/test/groovy/org/onap/cps/temporal/controller/event/listener/kafka/EventFixtures.groovy
+++ src/test/groovy/org/onap/cps/temporal/controller/event/listener/kafka/EventFixtures.groovy
@@ -1,18 +1,21 @@
 /*
  * ============LICENSE_START=======================================================
  * Copyright (c) 2021 Bell Canada.
+ * Modifications Copyright (C) 2021 Nordix Foundation.
  * ================================================================================
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
  *
- *        http://www.apache.org/licenses/LICENSE-2.0
+ *         http://www.apache.org/licenses/LICENSE-2.0
  *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
  * limitations under the License.
+ *
+ * SPDX-License-Identifier: Apache-2.0
  * ============LICENSE_END=========================================================
  */
 
@@ -32,36 +35,40 @@ class EventFixtures {
 
     static DateTimeFormatter isoTimestampFormatter = DateTimeFormatter.ofPattern("yyyy-MM-dd'T'HH:mm:ss.SSSZ")
     static String defaultEventType = 'org.onap.cps.data-updated-event'
-    static URI defaultEventSchema = new URI('urn:cps:org.onap.cps:data-updated-event-schema:v1')
+    static URI defaultEventSchema = new URI('urn:cps:org.onap.cps:data-updated-event-schema:v2')
     static URI defaultEventSource = new URI('urn:cps:org.onap.cps')
 
-    static CpsDataUpdatedEvent buildEvent(final Map map) {
-        CpsDataUpdatedEvent event =
-                new CpsDataUpdatedEvent()
-                        .withSchema(
-                                map.eventSchema != null ? new URI(map.eventSchema.toString()) : defaultEventSchema)
-                        .withId(
-                                map.id != null ? map.id.toString() : UUID.randomUUID().toString())
-                        .withType(
-                                map.eventType != null ? map.eventType.toString() : defaultEventType)
-                        .withSource(
-                                map.eventSource != null ? new URI(map.eventSource.toString()) : defaultEventSource)
-                        .withContent(
-                                new Content()
-                                        .withObservedTimestamp(
-                                                map.timestamp != null ? map.timestamp.toString() : currentTimestamp())
-                                        .withDataspaceName(
-                                                map.dataspace != null ? map.dataspace.toString() : 'a-dataspace')
-                                        .withSchemaSetName(
-                                                map.schemaSet != null ? map.schemaSet.toString() : 'a-schema-set')
-                                        .withAnchorName(
-                                                map.anchor != null ? map.anchor.toString() : 'an-anchor')
-                                        .withData(
-                                                new Data().withAdditionalProperty(
-                                                        map.dataName != null ? map.dataName.toString() : 'a-data-name',
-                                                        map.dataValue != null ? map.dataValue : 'a-data-value')))
+    static def defaultEventValue = [
+            eventSchema      : defaultEventSchema,
+            id               : UUID.randomUUID().toString(),
+            eventType        : defaultEventType,
+            eventSource      : defaultEventSource
+    ]
 
-        return event
+    static CpsDataUpdatedEvent buildEvent(final Map inputMap) {
+        def mergedMap = defaultEventValue + inputMap
+
+        def dataExist = mergedMap.containsKey('data')
+        Data data = null
+        if (dataExist) {
+            data = new Data()
+            mergedMap.data.each { k, v -> data.withAdditionalProperty(k, v) }
+        }
+
+        def content = new Content()
+                .withObservedTimestamp(mergedMap.observedTimestamp)
+                .withDataspaceName(mergedMap.dataspace)
+                .withSchemaSetName(mergedMap.schemaSet)
+                .withAnchorName(mergedMap.anchor)
+                .withOperation(mergedMap.operation)
+                .withData(data)
+
+        return new CpsDataUpdatedEvent()
+                .withSchema(mergedMap.eventSchema)
+                .withId(mergedMap.id)
+                .withType(mergedMap.eventType)
+                .withSource(mergedMap.eventSource)
+                .withContent(content)
     }
 
     static String currentIsoTimestamp() {
--- src/test/groovy/org/onap/cps/temporal/controller/event/model/CpsDataUpdatedEventMapperSpec.groovy
+++ src/test/groovy/org/onap/cps/temporal/controller/event/model/CpsDataUpdatedEventMapperSpec.groovy
@@ -6,13 +6,15 @@
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
  *
- *        http://www.apache.org/licenses/LICENSE-2.0
+ *         http://www.apache.org/licenses/LICENSE-2.0
  *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
  * limitations under the License.
+ *
+ * SPDX-License-Identifier: Apache-2.0
  * ============LICENSE_END=========================================================
  */
 
@@ -24,6 +26,7 @@ import org.onap.cps.event.model.Content
 import org.onap.cps.event.model.CpsDataUpdatedEvent
 import org.onap.cps.event.model.Data
 import org.onap.cps.temporal.domain.NetworkData
+import org.onap.cps.temporal.domain.Operation
 import spock.lang.Specification
 
 import java.time.OffsetDateTime
@@ -75,7 +78,7 @@ class CpsDataUpdatedEventMapperSpec extends Specification {
         then: 'the result entity is not null'
             result != null
         and: 'the result entity payload is an empty json '
-            result.getPayload() == "{}"
+            result.getPayload() == '{}'
     }
 
     def 'Mapping an event whose content data is invalid'() {
@@ -103,20 +106,33 @@ class CpsDataUpdatedEventMapperSpec extends Specification {
                                             .withDataspaceName('a-dataspace')
                                             .withSchemaSetName('a-schema-set')
                                             .withAnchorName('an-anchor')
+                                            .withOperation(Content.Operation.CREATE)
                                             .withData(new Data().withAdditionalProperty(aDataName, aDataValue)))
         when: 'the event is mapped to an entity'
             NetworkData result = objectUnderTest.eventToEntity(event)
         then: 'the result entity is not null'
             result != null
         and: 'all result entity properties are the ones from the event'
-            result.getObservedTimestamp() ==
-                OffsetDateTime.parse(event.getContent().getObservedTimestamp(), isoTimestampFormatter)
-            result.getDataspace() == event.getContent().getDataspaceName()
-            result.getSchemaSet() == event.getContent().getSchemaSetName()
-            result.getAnchor() == event.getContent().getAnchorName()
+            with(result) {
+                observedTimestamp ==
+                    OffsetDateTime.parse(event.getContent().getObservedTimestamp(), isoTimestampFormatter)
+                dataspace == event.getContent().getDataspaceName()
+                schemaSet == event.getContent().getSchemaSetName()
+                operation == Operation.CREATE
+                anchor == event.getContent().getAnchorName()
+                createdTimestamp == null
+            }
             result.getPayload().contains(aDataValue)
             result.getPayload().contains(aDataValue)
-            result.getCreatedTimestamp() == null
+    }
+
+    def 'Mapping event without operation field' () {
+        given: 'event without operation field in content'
+            def cpsDataUpdatedEvent = new CpsDataUpdatedEvent().withContent(new Content())
+        when: 'event is mapped to network data'
+            def networkData = objectUnderTest.eventToEntity(cpsDataUpdatedEvent)
+        then: 'the operation field has default UPDATE value'
+            networkData.operation == Operation.UPDATE
     }
 
     private void assertEntityPropertiesAreNull(NetworkData networkData) {
