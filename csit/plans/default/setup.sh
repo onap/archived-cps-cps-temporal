5e3924ef5f6285d1ab4241f0c9ff9b538c470827
Bruno Sakoto <bruno.sakoto@bell.ca>
2022-03-03 12:57:42 -0500

    Make a dummy change to have csit tests failing
    Change made to reproduce the bug to be fixed
    
    Issue-ID: CPS-905
    Signed-off-by: Bruno Sakoto <bruno.sakoto@bell.ca>
    Change-Id: I5f4c8073d2a992cba9eade9e1b7a2d1484bd1548

--- csit/plans/default/setup.sh
+++ csit/plans/default/setup.sh
@@ -31,6 +31,7 @@ curl -L https://github.com/docker/compose/releases/download/1.25.0/docker-compos
 chmod +x docker-compose
 
 # start CPS Temporal, cps-core, timescaledb, PostgresSQL and kafka containers with docker compose
+export CPS_TEMPORAL_DOCKER_REPO=dummy/
 ./docker-compose up -d
 python3 --version
 # Validate CPS service initialization completed via periodic log checking for line like below:
