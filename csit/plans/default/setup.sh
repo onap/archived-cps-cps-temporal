7ed959654b6a1680dd568b072ccf662b80a8c7d6
Bruno Sakoto <bruno.sakoto@bell.ca>
2022-03-16 22:55:10 -0400

    Update cps core version to 3.0.0
    Issue-ID: CPS-933
    Signed-off-by: Bruno Sakoto <bruno.sakoto@bell.ca>
    Change-Id: I1ef3ed96871c28ccbbde575af2a5c263000db39d

--- csit/plans/default/setup.sh
+++ csit/plans/default/setup.sh
@@ -32,7 +32,7 @@ chmod +x docker-compose
 
 # Set environment variables for docker compose
 export CPS_TEMPORAL_DOCKER_REPO=
-export STABLE_CPS_CORE_VERSION=3.0.0-SNAPSHOT-20220303T233031Z
+export STABLE_CPS_CORE_VERSION=3.0.0
 # start CPS Temporal, cps-core, timescaledb, PostgresSQL and kafka containers with docker compose
 ./docker-compose up -d
 python3 --version
